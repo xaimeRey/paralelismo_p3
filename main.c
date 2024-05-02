b3d10711aca05ccea276a06cb3496f5df00ffb9e
xaime.rey <xaime.rey@udc.es>
2024-05-02 14:40:27 +0200

    tiempos y comentado

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <mpi.h>
 #include <math.h>
 
-#define N 6
+#define N 11
 
 void printMatrix(int rank, int n, float *matrix)
 {
@@ -30,6 +30,12 @@ void printVector(int rank, int n, float v[])
         printf("%f ", v[i]);
     }
     printf("\n\n");
+}
+
+void printTiempos(int rank, char *str, struct timeval tv1, struct timeval tv2)
+{
+    int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
+    printf("rank: %d -> %s = %lf\n", rank, str, (double)microseconds / 1E6);
 }
 
 int main(int argc, char *argv[])
@@ -78,12 +84,13 @@ int main(int argc, char *argv[])
                 matrix[i * N + j] = i + j;
             }
         }
-
+        // sendcount = nº elementos para cada proceso
         for (int i = 0; i < numprocs; i++)
         {
             sendcountS[i] = ((N / numprocs) + (i < N % numprocs ? 1 : 0)) * N; // filas_indiv pero calculadas por el proceso 0 ya que no tiene acceso a filas_indiv de cada proceso, luego multiplica por N porque son filas por N columnas
             sendcountG[i] = ((N / numprocs) + (i < N % numprocs ? 1 : 0));
         }
+        // displs = desplazamiento en el array del que cogemos elementos para empezar a enviar al siguiente proceso
         displsS[0] = 0;
         displsG[0] = 0;
         for (int i = 1; i < numprocs; i++)
@@ -92,11 +99,16 @@ int main(int argc, char *argv[])
             displsG[i] = displsG[i - 1] + sendcountG[i - 1];
         }
     }
+    // COMUNICACIONES SCATTERV
+    gettimeofday(&tv1, NULL);
     MPI_Scatterv(matrix, sendcountS, displsS, MPI_FLOAT, matrixAux, filas_indiv * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
     MPI_Bcast(vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    gettimeofday(&tv2, NULL);
 
+    printTiempos(rank, "Comunicación Scatterv (segundos)", tv1, tv2);
+
+    // CÁLCULOS
     gettimeofday(&tv1, NULL);
-
     for (i = 0; i < filas_indiv; i++)
     {
         resultAux[i] = 0;
@@ -105,13 +117,14 @@ int main(int argc, char *argv[])
             resultAux[i] += matrixAux[i * N + j] * vector[j];
         }
     }
-
     gettimeofday(&tv2, NULL);
+    printTiempos(rank, "Cálculos (segundos)", tv1, tv2);
 
-    int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
-
+    // COMUNICACIÓN GATHERV
+    gettimeofday(&tv1, NULL);
     MPI_Gatherv(resultAux, filas_indiv, MPI_FLOAT, result, sendcountG, displsG, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    printf("Time (seconds) = %lf\n", (double)microseconds / 1E6);
+    gettimeofday(&tv2, NULL);
+    printTiempos(rank, "Comunicación Gatherv (segundos)", tv1, tv2);
 
     /*Display result */
 
