44daff2065f98c25d12e8dc979763d17cfeb4a67
xaime.rey <xaime.rey@udc.es>
2024-05-02 14:19:03 +0200

    acabado

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include <mpi.h>
 #include <math.h>
 
-#define N 5
+#define N 6
 
-void print_matrix1(int rank, int n, float *matrix)
+void printMatrix(int rank, int n, float *matrix)
 {
     int i, j;
     printf("---------------%d-----------------\n", rank);
@@ -21,7 +21,7 @@ void print_matrix1(int rank, int n, float *matrix)
     printf("\n");
 }
 
-void print_vector(int rank, int n, float v[])
+void printVector(int rank, int n, float v[])
 {
     int i;
     printf("-------%d---------\n", rank);
@@ -32,42 +32,25 @@ void print_vector(int rank, int n, float v[])
     printf("\n\n");
 }
 
-void print_matrix2(int rank, int n, float matrix[][N])
-{
-    int i, j;
-    printf("---------------%d-----------------\n", rank);
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            printf("%f ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
 int main(int argc, char *argv[])
 {
 
-    int i, j, rank, numprocs, filas, filas_extra;
-    float *matrix, *matrixAux;
+    int i, j, rank, numprocs, filas, filas_indiv;
+    float *matrix, *result, *matrixAux, *resultAux;
+    int *sendcountS, *sendcountG, *displsS, *displsG;
     float vector[N];
-    float *result, *resultAux;
     struct timeval tv1, tv2;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    filas = ceil(N / numprocs);
-    filas_extra = (N % numprocs != 0 && rank == numprocs - 1) ? (numprocs * filas - N) : 0;
+    filas = N / numprocs;
+    filas_indiv = filas + (rank < N % numprocs ? 1 : 0);
 
-    printf("%d-----%d\n", filas, filas_extra);
-
-    if ((matrixAux = malloc(sizeof(float) * (filas + filas_extra) * N)) == NULL)
+    if ((matrixAux = malloc(sizeof(float) * filas_indiv * N)) == NULL)
         perror("error 1: ");
-    if ((resultAux = malloc(sizeof(float) * (filas + filas_extra))) == NULL)
+    if ((resultAux = malloc(sizeof(float) * filas_indiv)) == NULL)
         perror("error 2: ");
 
     /* Initialize Matrix and Vector */
@@ -77,6 +60,14 @@ int main(int argc, char *argv[])
             perror("error 3: ");
         if ((result = malloc(sizeof(float) * N)) == NULL)
             perror("error 4: ");
+        if ((sendcountS = malloc(sizeof(int) * numprocs)) == NULL)
+            perror("error 5: ");
+        if ((sendcountG = malloc(sizeof(int) * numprocs)) == NULL)
+            perror("error 6: ");
+        if ((displsS = malloc(sizeof(int) * numprocs)) == NULL)
+            perror("error 7: ");
+        if ((displsG = malloc(sizeof(int) * numprocs)) == NULL)
+            perror("error 8: ");
 
         for (int i = 0; i < N; i++)
         {
@@ -87,22 +78,27 @@ int main(int argc, char *argv[])
                 matrix[i * N + j] = i + j;
             }
         }
-        print_matrix1(rank, N, matrix);
+
+        for (int i = 0; i < numprocs; i++)
+        {
+            sendcountS[i] = ((N / numprocs) + (i < N % numprocs ? 1 : 0)) * N; // filas_indiv pero calculadas por el proceso 0 ya que no tiene acceso a filas_indiv de cada proceso, luego multiplica por N porque son filas por N columnas
+            sendcountG[i] = ((N / numprocs) + (i < N % numprocs ? 1 : 0));
+        }
+        displsS[0] = 0;
+        displsG[0] = 0;
+        for (int i = 1; i < numprocs; i++)
+        {
+            displsS[i] = displsS[i - 1] + sendcountS[i - 1];
+            displsG[i] = displsG[i - 1] + sendcountG[i - 1];
+        }
     }
-    MPI_Scatter(matrix, N * filas, MPI_FLOAT, matrixAux, N * filas, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(matrix, sendcountS, displsS, MPI_FLOAT, matrixAux, filas_indiv * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
     MPI_Bcast(vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    if (rank != 2)
-        print_matrix1(rank, filas, matrixAux);
-    else
-        print_matrix1(rank, filas + (numprocs * filas - N), matrixAux);
-
-    // COMUNICADO ???
     gettimeofday(&tv1, NULL);
 
-    for (i = 0; i < filas; i++)
+    for (i = 0; i < filas_indiv; i++)
     {
-
         resultAux[i] = 0;
         for (j = 0; j < N; j++)
         {
@@ -114,7 +110,7 @@ int main(int argc, char *argv[])
 
     int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
 
-    MPI_Gather(resultAux, filas, MPI_FLOAT, result, filas, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(resultAux, filas_indiv, MPI_FLOAT, result, sendcountG, displsG, MPI_FLOAT, 0, MPI_COMM_WORLD);
     printf("Time (seconds) = %lf\n", (double)microseconds / 1E6);
 
     /*Display result */
@@ -127,8 +123,11 @@ int main(int argc, char *argv[])
         }
         free(matrix);
         free(result);
+        free(sendcountG);
+        free(sendcountS);
+        free(displsG);
+        free(displsS);
     }
-
     free(matrixAux);
     free(resultAux);
 
